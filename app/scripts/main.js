653d21d4b2825426cd30cc1229fc4e9f08281688
varunumesh <varun@zysk.in>
2019-10-20 00:02:15 +0530

    Fix: Responsiveness Home page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,7 @@ console.log('\'Allo \'Allo!');
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+
+$('.service-thumb').hover(function () {
+  $(this).toggleClass('shadow-lg rounded-lg');
+});
