cc65b3f0168ba9662cfdac32021d670375665c8a
varunumesh <varun@zysk.in>
2021-05-30 01:32:26 +0530

    Footer update
    What:
    Used Javascript date function to update the year in the footer.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,40 +10,43 @@ $('.service-thumb').hover(function () {
   $(this).toggleClass('shadow-lg rounded-lg');
 });
 
+var d = new Date();
+document.getElementById('year').innerHTML = d.getFullYear();
+
 // set up text to print, each item in array is new line
 var aText = new Array(
   'We get our geek hats on',
   'and code your ideas to life.'
-  )
-  var iSpeed = 100; // time delay of print out
-  var iIndex = 0; // start printing array at this posision
-  var iArrLength = aText[0].length; // the length of the text array
-  var iScrollAt = 20; // start scrolling up at this many lines
+);
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
 
-  var iTextPos = 0; // initialise text position
-  var sContents = ''; // initialise contents variable
-  var iRow; // initialise current row
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
 
-  function typewriter()
-  {
-   sContents =  ' ';
-   iRow = Math.max(0, iIndex-iScrollAt);
-   var destination = document.getElementById('typedtext');
+function typewriter() {
+  sContents = ' ';
+  iRow = Math.max(0, iIndex - iScrollAt);
+  var destination = document.getElementById('typedtext');
 
-   while ( iRow < iIndex ) {
+  while (iRow < iIndex) {
     sContents += aText[iRow++] + '<br />';
-   }
-   destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + '|';
-   if ( iTextPos++ == iArrLength ) {
+  }
+  destination.innerHTML =
+    sContents + aText[iIndex].substring(0, iTextPos) + '|';
+  if (iTextPos++ == iArrLength) {
     iTextPos = 0;
     iIndex++;
-    if ( iIndex != aText.length ) {
-     iArrLength = aText[iIndex].length;
-     setTimeout('typewriter()', 500);
+    if (iIndex != aText.length) {
+      iArrLength = aText[iIndex].length;
+      setTimeout('typewriter()', 500);
     }
-   } else {
+  } else {
     setTimeout('typewriter()', iSpeed);
-   }
   }
+}
 
-  typewriter();
+typewriter();
